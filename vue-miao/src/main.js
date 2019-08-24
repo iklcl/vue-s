ff02083280279a8d288442410d4069018bdb8edc
ikcl <2529525986@qq.com>
2019-08-24 17:16:13 +0800

    搜索完成

--- vue-miao/src/main.js
+++ vue-miao/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
 import store from './stores'
-
+import axios from 'axios'
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
